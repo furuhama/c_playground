9b70eb0948b551fee6ba2c21dcfc4f9196b6e589
furuhama <furuhama@users.noreply.github.com>
2018-09-30 22:44:54 +0900

    Fix command line echo srcipt

--- src/commandLine.c
+++ src/commandLine.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char *argv[]) {
     if (argc <= 2) {
-        printf("Usage: ./commandline_test [some words]");
+        printf("Usage: ./commandline_test [some words]\n");
         return 1;
     } else {
         for (int i = 1; i < argc; i++) {
-            printf("%s ", argv[i]);
+            printf("%d: %s\n", i, argv[i]);
         }
         return 0;
     }
