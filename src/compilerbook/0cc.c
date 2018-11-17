6fe96fcb4d48d8a636a9680903a7d90b1d914711
furuhama <furuhama@users.noreply.github.com>
2018-11-17 20:06:54 +0900

    Update 0cc to take `+` and `-` operators

--- src/compilerbook/0cc.c
+++ src/compilerbook/0cc.c
@@ -7,10 +7,33 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global _main\n");
     printf("_main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+
+    // `strtol` returns `long` type value and get it by `%ld`
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character: '%c'\n", *p);
+
+        return 1;
+    }
+
     printf("    ret\n");
 
     return 0;
